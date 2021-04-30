d7850857e856a4cccccf1764fd8b50b774f6aba0
Jonnibones <js5800672@gmail.com>
2021-04-29 22:28:33 -0300

    criado o formulário e a tabela no bd

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="UTF-8" name="viewport" content="width=device-width,
+     initial-scale=1">
+
+<link href="_bootstrap/bootstrap.min.css"
+rel="stylesheet" type="text/css">
+
+<script type="text/javascript"
+src="_js/jquery.js"></script>
+
+   <script type="text/javascript"
+src="_js/bootstrap.min.js"></script>
+
+    <title>Cadastro de funcionários</title>
+  </head>
+
+  <body>
+
+  	<div class="container-md" style="background-color: black;">
+  		
+  		<h1 style="color: white; text-align: center;">Cadastro de funcionários</h1>
+
+  	</div>
+
+  	<div class="container-md">
+    <form>
+
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nome</label>
+    <input type="text" class="form-control" name="nome" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">E-mail</label>
+    <input type="email" name="email" class="form-control" id="exampleInputPassword1">
+  </div>
+
+  <button type="submit" class="btn btn-success">Cadastrar</button>
+
+</form>
+</div>
+
+  </body>
+
+</html>
