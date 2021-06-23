d07f13356738d23b5c4cf9aee62d2e5e83cefd9f
Jonnibones <js5800672@gmail.com>
2021-06-22 22:26:57 -0300

    Terminando o projeto, faltando apenas os comentário e tratamento de erros

--- index.php
+++ index.php
@@ -1,58 +1,73 @@
 <?php 
-
+//Carrega o cabeçalho
 require_once("interface/header.php");
+//Carrega a página config.php
 require_once("config.php");
+//Inicia a sessão
 session_start();
-
+/*Verifica se a variável Super global está definida.
+Caso não esteja ela inicia com os valores abaixo para o texto e tipo do botão de cadastro*/
 if(!isset($_SESSION['btntype']) ){
 $_SESSION['btntype'] = "btn btn-success";
 $_SESSION['btnname'] = "Cadastrar";
 }
-
  ?>
 
-  	<main>
+<main>
+<!--Div para mensagem para quando uma ação é executada -->
+<div class="<?php echo $_SESSION['msgcolor']?>"><p><?php
+  error_reporting(E_ALL ^ E_NOTICE);
+  echo $_SESSION['msg'];
+  unset($_SESSION['msg']);
+  unset($_SESSION['msgcolor']);
+?></p></div>
 
   	<!--Formulário-->
-  	<div class="container-md">
-    <form method="POST" action="processa.php">
-
-  <div class="mb-3">
+<div class="container-md">
+  <form method="POST" action="processa.php">
+    <div class="mb-3">
     <label class="form-label">Nome</label>
-    <input type="text" class="form-control" value="<?php error_reporting(E_ALL ^ E_NOTICE); echo $_SESSION['valor'][0]; ?>" id="id_name" name="inp_nome">
-  </div>
-
-  <div class="mb-3">
+    <input required type="text" class="form-control" value="<?php error_reporting(E_ALL ^ E_NOTICE); echo $_SESSION['valor'][0]; ?>" id="id_name" name="inp_nome">
+    </div>
+    <div class="mb-3">
     <label class="form-label">E-mail</label>
-    <input type="email" name="inp_email" class="form-control" value="<?php error_reporting(E_ALL ^ E_NOTICE); echo $_SESSION['valor'][1]; ?>" id="id_email">
-  </div>
-
-  <button type="submit" name="btn_enviar" class="<?php echo $_SESSION['btntype']?>"><?php echo $_SESSION['btnname']?></button>
-  
-</form>
+    <input required type="email" name="inp_email" class="form-control" value="<?php error_reporting(E_ALL ^ E_NOTICE); echo $_SESSION['valor'][1]; ?>" id="id_email">
+    </div>
+    <button type="submit" name="btn_enviar"class="<?php echo $_SESSION['btntype']?>"><?php echo $_SESSION['btnname']?></button>
+  </form>
 </div>
 
 <!--Tabela-->
- <div class="container-md">
-   <table id="tab" class="table">
+  <div class="container-md">
+    <table id="tab" class="table">
 
-    <tr>
-      <th>Id Funcionário</th>
-      <th>Nome Funcionário</th>
-      <th>Email Funcionário</th>
-      <th>Ação</th>
-    </tr>
+<!--Colunas-->
+      <tr>
+        <th>Id Funcionário</th>
+        <th>Nome Funcionário</th>
+        <th>Email Funcionário</th>
+        <th>Ação</th>
+      </tr>
+
 
     <?php
+    //Instância da classe
     $lista = new Usuario();
+    //Chamada para o método
     $listagem = $lista->getList();
-
+    //Contador que incrementa durante o laço foreach
     $num = 0;
+    //Variável que inicia como string
     $lista = "";
+    //Verifica se a variável não retorna NULL(se o array com os dados da tabela não está vazio)
+   if($listagem != NULL){
+    //Caso verdadeiro, enquanto o array é percorrido estará sendo imprimida uma linha com os valores de cada coluna da tabela.  
     foreach ($listagem as $value) {
+     //Variáveis que recebem valores da tabela, para serem passados via GET
      $id = $listagem[$num][0];
      $nome = $listagem[$num][1];
      $email = $listagem[$num][2];
+     //Linhas, colunas com valores do banco de dados e botões para ação
      $lista .= "<tr>
      <td>" .$listagem[$num][0]. "</td>
      <td>" .$listagem[$num][1]. "</td>
@@ -60,16 +75,18 @@ $_SESSION['btnname'] = "Cadastrar";
      <td><a class='btn btn-info' href='processa.php?edit=$nome,$email,$id'>Editar</a>
      <a class='btn btn-danger' href='processa.php?delete=$id'>Deletar</a></td>
      </tr>";
+     //Incremento do contador
      $num++;
     }
-    if ($listagem === NULL) {
-      echo "Nenhum registro";
-    }
+    //Imprime os registros contidos na variável
     echo $lista;
+    //Caso a variável $listagem retorne NULL
+  }else{
+    echo "<h3>Sem registros na tabela.</h3>";
+  }
     ?>
-
-</table>
-</div>
+    </table>
+  </div>
 </main>
   </body>
 
--- processa.php
+++ processa.php
@@ -4,20 +4,29 @@ require_once("config.php");
 
 //Inserir e editar usuario
 if (isset($_POST['btn_enviar'])) {
-
 	if ($_SESSION['btntype'] == "btn btn-info") {
 		$usuario = new Usuario();
 	$usuario->updateUsuario($_SESSION['valor'][2],$_POST['inp_nome'], $_POST['inp_email']);
+	$_SESSION['msg'] = "Registro editado.";
+	$_SESSION['msgcolor'] = "alert alert-warning";
 	unset($_SESSION['btntype']);
 	unset($_SESSION['btnname']);
 	unset($_SESSION['valor']);
 	header("location:index.php");
 	}else{
+		if(!empty($_POST['inp_nome'] and !empty($_POST['inp_email']))){
 	$usuario = new Usuario();
 	$usuario->insertUsuario($_POST['inp_nome'], $_POST['inp_email']);
+	$_SESSION['msg'] = "Registro inserido.";
+	$_SESSION['msgcolor'] = "alert alert-success";
 	unset($_SESSION['btntype']);
 	unset($_SESSION['btnname']);
 	header("location:index.php");
+}else{
+	$_SESSION['msg'] = "Campos não podem ser vazios!";
+	$_SESSION['msgcolor'] = "alert alert-danger";
+	header("location:index.php");
+}
 }
 }
 
@@ -25,6 +34,8 @@ if (isset($_POST['btn_enviar'])) {
 if (isset($_GET['delete'])) {
 	$deletar = new Usuario();
 	$deletar->deleteUsuario($_GET['delete']);
+	$_SESSION['msg'] = "Registro excluído.";
+	$_SESSION['msgcolor'] = "alert alert-danger";
 	header("location:index.php");
 }
 
